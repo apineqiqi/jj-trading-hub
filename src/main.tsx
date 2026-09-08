4adfceca7c3b777d9a5c0da6375118d519c81bb1
apineqiqi <apineqiqi@gmail.com>
2026-09-08 18:04:03 +0800

    Add V1.4 strategy import integration and styling

--- src/main.tsx
+++ src/main.tsx
@@ -4,5 +4,6 @@ import App from './App';
 import './styles.css';
 import './v12.css';
 import './v13.css';
+import './v14.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>);
